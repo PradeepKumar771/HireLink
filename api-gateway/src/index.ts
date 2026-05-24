4ff2b205c7fa057dc4af32bedb766076f4150b9d
PradeepKumar771 <pk5373861@gmail.com>
2026-05-24 22:05:58 +0530

    Allow all preflight headers in CORS options to bulletproof cross-origin requests

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -20,8 +20,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id', 'x-user-role', 'x-user-college-id', 'x-user-email', 'x-user-name']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
 
 // Preflight OPTIONS handler to guarantee CORS checks always succeed immediately
