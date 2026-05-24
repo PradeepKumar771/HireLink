3a2f3e3e172b6c296a68ba5fb08df11a476728d4
PradeepKumar771 <pk5373861@gmail.com>
2026-05-24 22:16:34 +0530

    Migrate Prisma generator targets to standard @prisma/client package and update service imports

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from './generated/client';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
